d6761787c88c781cc768acc0f17945032b0ad168
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-03-28 08:54:27 +0100

    added module documentation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
+'''Reads stdin line by line and parses http request logs'''
 import sys
 import re
 
-# Creates a dictionary that stores file size and counts of HTTP status codes
-
 
 def initialize_log():
+    '''
+    Creates a dictionary that stores file size and counts of HTTP status codes
+    '''
     status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
     log = {'file_size': 0, 'code_list': {
         str(code): 0 for code in status_codes}}
     return log
 
-# Processes the log by using regular expressions to find a match.
-# updates the log dict file
-
 
 def parse_line(line, regex, log):
+    '''
+    Processes the log by using regular expressions to find a match.
+    updates the log dict file
+    '''
     match = regex.fullmatch(line)
 
     if match:
@@ -29,10 +31,9 @@ def parse_line(line, regex, log):
 
     return log
 
-# prints out the file size and status codes in the required format
-
 
 def print_codes(log):
+    '''prints out the file size and status codes in the required format'''
     print("File size: {}".format(log['file_size']))
 
     sorted_status_codes = sorted(log['code_list'])
@@ -41,10 +42,9 @@ def print_codes(log):
         if log['code_list'][code]:
             print(f"{code}: {log['code_list'][code]}")
 
-# entry point
-
 
 def main():
+    '''entry point'''
     regex = re.compile(
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
 
