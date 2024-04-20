b98b3847132586dc9c6019e36b044f2ffd9c4372
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-04-20 14:47:19 +0100

    changed the implementation

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,59 @@
-#!/usr/bin/python3
-""" N queens """
+#!/usr/bin/env python3
+'''N queens'''
 import sys
+from typing import List
+
+args = sys.argv
+
+if len(args) < 2:
+    print('Usage: nqueens N\n')
+    sys.exit(1)
+
+if not args[1].isdecimal():
+    print('N must be a number\n')
+    sys.exit(1)
+
+args[1] = int(args[1])
+
+if args[1] < 4:
+    print('N must be at least 4\n')
+    sys.exit(1)
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def nqueens(n: int) -> List[List[int]]:
+    '''finds spots where a queen can be placed'''
+    col = set()
+    posDiag = set()
+    negDiag = set()
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+    res = []
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
+    def backtrack(r, board):
+        if r == n:
+            res.append(board)
+            return
 
-n = int(sys.argv[1])
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
 
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+            new_board = board + [[r, c]]
+            backtrack(r + 1, new_board)
 
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    backtrack(0, [])
+
+    return res
 
 
-solve(n)
+solutions = nqueens(args[1])
+
+for solution in solutions:
+    print(solution)
